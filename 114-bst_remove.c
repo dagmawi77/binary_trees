55b279c03d8229b911b859e3fa58bc4260e9d364
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-03-08 23:26:36 +0300

    113-bst_search.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -4,11 +4,9 @@ bst_t *inorder_successor(bst_t *root);
 bst_t *bst_delete(bst_t *root, bst_t *node);
 bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value);
 bst_t *bst_remove(bst_t *root, int value);
-
 /**
  * inorder_successor - Returns the minimum value of a binary search tree.
  * @root: A pointer to the root node of the BST to search.
- *
  * Return: The minimum value in @tree.
  */
 bst_t *inorder_successor(bst_t *root)
@@ -17,12 +15,10 @@ bst_t *inorder_successor(bst_t *root)
 		root = root->left;
 	return (root);
 }
-
 /**
  * bst_delete - Deletes a node from a binary search tree.
  * @root: A pointer to the root node of the BST.
  * @node: A pointer to the node to delete from the BST.
- *
  * Return: A pointer to the new root node after deletion.
  */
 bst_t *bst_delete(bst_t *root, bst_t *node)
@@ -61,13 +57,11 @@ bst_t *bst_delete(bst_t *root, bst_t *node)
 
 	return (bst_delete(root, successor));
 }
-
 /**
  * bst_remove_recursive - Removes a node from a binary search tree recursively.
  * @root: A pointer to the root node of the BST to remove a node from.
  * @node: A pointer to the current node in the BST.
  * @value: The value to remove from the BST.
- *
  * Return: A pointer to the root node after deletion.
  */
 bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value)
@@ -82,16 +76,12 @@ bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value)
 	}
 	return (NULL);
 }
-
 /**
  * bst_remove - Removes a node from a binary search tree.
  * @root: A pointer to the root node of the BST to remove a node from.
  * @value: The value to remove in the BST.
- *
  * Return: A pointer to the new root node after deletion.
- *
- * Description: If the node to be deleted has two children, it
- *              is replaced with its first in-order successor.
+ * Description: If the node to be deleted has two children.
  */
 bst_t *bst_remove(bst_t *root, int value)
 {

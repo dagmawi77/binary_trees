252e086735d4c3a11697c78d19a424be0b5fa064
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-03-08 23:12:01 +0300

    101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -6,13 +6,10 @@ void pint_push(binary_tree_t *node, levelorder_queue_t *head,
 		levelorder_queue_t **tail, void (*func)(int));
 void pop(levelorder_queue_t **head);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
-
 /**
  * create_node - Creates a new levelorder_queue_t node.
  * @node: The binary tree node for the new node to contain.
- *
  * Return: If an error occurs, NULL.
- *         Otherwise, a pointer to the new node.
  */
 levelorder_queue_t *create_node(binary_tree_t *node)
 {
@@ -27,7 +24,6 @@ levelorder_queue_t *create_node(binary_tree_t *node)
 
 	return (new);
 }
-
 /**
  * free_queue - Frees a levelorder_queue_t queue.
  * @head: A pointer to the head of the queue.
@@ -43,15 +39,12 @@ void free_queue(levelorder_queue_t *head)
 		head = tmp;
 	}
 }
-
 /**
- * pint_push - Runs a function on a given binary tree node and
- *             pushes its children into a levelorder_queue_t queue.
+ * pint_push - Runs a function on a given binary tree node.
  * @node: The binary tree node to print and push.
  * @head: A double pointer to the head of the queue.
  * @tail: A double pointer to the tail of the queue.
  * @func: A pointer to the function to call on @node.
- *
  * Description: Upon malloc failure, exits with a status code of 1.
  */
 void pint_push(binary_tree_t *node, levelorder_queue_t *head,
@@ -83,7 +76,6 @@ void pint_push(binary_tree_t *node, levelorder_queue_t *head,
 		*tail = new;
 	}
 }
-
 /**
  * pop - Pops the head of a levelorder_queue_t queue.
  * @head: A double pointer to the head of the queue.
@@ -96,10 +88,8 @@ void pop(levelorder_queue_t **head)
 	free(*head);
 	*head = tmp;
 }
-
 /**
- * binary_tree_levelorder - Traverses a binary tree using
- *                          level-order traversal.
+ * binary_tree_levelorder - Traverses a binary tree.
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  */
